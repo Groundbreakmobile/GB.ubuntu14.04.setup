b12acbe36cbf4a16a21e010517d77c940a541358
NSM GB Server <nmorris@groundbreakmobile.com>
2016-01-26 00:08:34 +0000

    112284277 : add apc extension

--- gb.instance.install.sh
+++ gb.instance.install.sh
@@ -121,7 +121,7 @@ cd extensions/
  pecl download apc
 tar vxzf APC-3.1.13.tgz
  cd APC-3.1.13/
- /opt/phpfarm/inst/bin/phpize-5.4.4
+ /opt/phpfarm/inst/bin/phpize-5.4.45
  ./configure --with-php-config=/opt/phpfarm/inst/bin/php-config-5.4.45
   make
   make install
