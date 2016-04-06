f33a6c9adeb144c62e5a5ba20e0a1e585a74cdbd
NSM GB Server <nmorris@groundbreakmobile.com>
2016-04-06 17:32:13 +0000

    adding composer

--- gb.instance.install.sh
+++ gb.instance.install.sh
@@ -159,3 +159,6 @@ chmod +x /usr/bin/repair
 echo "set git publishing credentials"
 git config --global user.name "NSM GB Server"
 git config --global user.email nmorris@groundbreakmobile.com
+
+echo "setting up composer"
+~/GB.ubuntu14.04.setup/add.composer.sh
